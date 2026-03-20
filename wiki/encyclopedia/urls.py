fc8deb81f60e3f1f764e377f0772ec28a0b26c31
rithekasudhakar3-sketch <rithekasudhakar3@gmail.com>
2026-03-20 12:59:09 +0530

    django

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>",views.entry,name="entry")
+    path("wiki/<str:title>",views.entry,name="entry"),
+    path("create", views.create, name="create"),
+    path("random", views.random, name="random")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 from markdown2 import Markdown
 from requests import request
 from . import util
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+
+class NewPageForm(forms.Form):
+    page = forms.CharField(label = "newpage")
+
 
 def md_to_html(title):
     content = util.get_entry(title)
@@ -25,3 +33,20 @@ def entry(request,title):
         return render(request,"encyclopedia/error.html")
     else:
         return render(request,"encyclopedia/entry.html")
+    
+def create(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data["page"]
+            # Save the new entry using util.save_entry or similar logic
+            util.save_entry(entry, "")  # You may want to add content from the form
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = NewPageForm()
+    return render(request, "encyclopedia/create_page.html", {"form": form})
+
+
+def random(request):
+    return render(request, "encyclopedia/random.html")
+
